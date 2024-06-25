d5ae8b4fb902e853f1301482e576f419fe39e98d
Taiyo <taiyoperson@gmail.com>
2024-06-25 14:41:42 +1200

    Progress

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function toggleMenu() {
     if (document.getElementById("hamburger-menu").style.width === "0%")
         {
           document.getElementById("hamburger-menu").style.width = "100%";
-        //   body.classList.add("fixed-position");
         }
         else
         {
           document.getElementById("hamburger-menu").style.width = "0%";
-        //   body.classList.remove("fixed-position");
         }
 }
