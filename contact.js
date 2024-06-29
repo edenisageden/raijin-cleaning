b2baf07e107e831bf5ea8ef667848b6ec9d33bb4
Taiyo <taiyoperson@gmail.com>
2024-06-30 00:01:49 +1200

    k

--- contact.js
+++ contact.js
@@ -1,8 +1,9 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const phone = document.getElementById('phone');
+const company = document.getElementById('company');
+const address = document.getElementById('address');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -36,11 +37,12 @@ const isValidEmail = email => {
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+    const phoneValue = phone.value.trim();
+    const addressValue = address.value.trim();
+    const companyValue = company.value.trim();
 
     if(usernameValue === '') {
-        setError(username, 'Username is required');
+        setError(username, 'Name is required');
     } else {
         setSuccess(username);
     }
@@ -53,20 +55,23 @@ const validateInputs = () => {
         setSuccess(email);
     }
 
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
+    if(phoneValue === '') {
+        setError(password, 'Phone number is required');
+    } else if (phoneValue.length < 10 ) {
+        setError(phone, 'Phone number must be at least 10 numbers.')
     } else {
-        setSuccess(password);
+        setSuccess(phone);
     }
 
-    if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
+    if(addressValue === '') {
+        setError(address, 'Please enter your address');
     } else {
-        setSuccess(password2);
+        setSuccess(address);
     }
 
+    if(companyValue === '') {
+        setError(company, 'Please enter your company');
+    } else {
+        setSuccess(company);
+    }
 };
