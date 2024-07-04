2acf493c8e912019e0a2e256d0a196e8816c35cb
Taiyo <taiyoperson@gmail.com>
2024-07-04 19:55:09 +1200

    c

--- script.js
+++ script.js
@@ -2,13 +2,10 @@
 
 // Hamburger menu
 function toggleMenu() {
-  if (document.getElementById("hamburger-menu").style.width === "0%") {
-    document.getElementById("hamburger-menu").style.width = "100%";
+  if (document.getElementById("hamburger-menu").style.width === "0%") { document.getElementById("hamburger-menu").style.width = "100%";
     document.getElementById("hamburger-menu__button__img").src = "images/cross.png";
-  } else {
-    document.getElementById("hamburger-menu").style.width = "0%";
-    document.getElementById("hamburger-menu__button__img").src = "images/hamburger-menu-white.png";
-  }
+  } else { document.getElementById("hamburger-menu").style.width = "0%";
+    document.getElementById("hamburger-menu__button__img").src = "images/hamburger-menu-white.png"; }
 }
 
 // Slideshow
