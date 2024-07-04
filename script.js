91d974d5cff31e44173544e3405397cb89963ee8
Taiyo <taiyoperson@gmail.com>
2024-07-04 19:50:57 +1200

    c

--- script.js
+++ script.js
@@ -32,16 +32,14 @@ var background = document.getElementById("banner");
 var title = document.getElementById("title");
 
 function nextImage() {
-  console.log("next");
-  i++;
+  i += 1;
   if (i > images.length - 1) i = 0;
   var imageNum = i + 1;
   background.style.backgroundImage = "url('images/banner" + imageNum + ".jpg')";
   title.innerText = titles[i];
 }
 function prevImage() {
-  console.log("prev");
-  i--;
+  i -= 1;
   if (i < 0) i = images.length - 1;
   var imageNum = i + 1;
   background.style.backgroundImage = "url('images/banner" + imageNum + ".jpg')";
