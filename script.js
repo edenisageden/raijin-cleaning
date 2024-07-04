9a6004d1f5a5e409ba619c3ca3f6b7ab57599311
Taiyo <taiyoperson@gmail.com>
2024-07-04 20:06:10 +1200

    c

--- script.js
+++ script.js
@@ -27,14 +27,12 @@ var title = document.getElementById("title");
 function nextImage() {
   i += 1;
   if (i > images.length - 1) { i = 0; }
-  var imageNum = i + 1;
-  background.style.backgroundImage = "url('images/banner" + imageNum + ".jpg')";
+  background.style.backgroundImage = "url('images/banner" + (i + 1) + ".jpg')";
   title.innerText = titles[i];
 }
 function prevImage() {
   i -= 1;
   if (i < 0) { i = images.length - 1; }
-  var imageNum = i + 1;
-  background.style.backgroundImage = "url('images/banner" + imageNum + ".jpg')";
+  background.style.backgroundImage = "url('images/banner" + (i + 1) + ".jpg')";
   title.innerText = titles[i];
 }
