1d76e01eba33215e6494deb165201ffbee76400f
Taiyo <taiyoperson@gmail.com>
2024-07-04 19:53:30 +1200

    c

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function toggleMenu() {
   if (document.getElementById("hamburger-menu").style.width === "0%") {
     document.getElementById("hamburger-menu").style.width = "100%";
     document.getElementById("hamburger-menu__button__img").src = "images/cross.png";
-  }
-  else {
+  } else {
     document.getElementById("hamburger-menu").style.width = "0%";
     document.getElementById("hamburger-menu__button__img").src = "images/hamburger-menu-white.png";
   }
